101478e5c8a4df1605ddc7c91fa829d84609956e
yair naor <ynaor12@gmail.com>
2021-12-15 14:26:02 +0200

    mwhahahahaha

--- PL1/ParcelListWindow.xaml.cs
+++ PL1/ParcelListWindow.xaml.cs
@@ -19,9 +19,22 @@ namespace PL1
     /// </summary>
     public partial class ParcelListWindow : Window
     {
+        BLAPI.IBL bl;
         public ParcelListWindow(BLAPI.IBL bl)
         {
+           
             InitializeComponent();
+            CustomerViewList.ItemsSource = bl.GetParcelList();
+        }
+
+        private void Closing_Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void CustomerViewList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
