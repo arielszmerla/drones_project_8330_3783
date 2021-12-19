f521e1a03e3863a7b7ecf3eb81daa05af464d5ae
Ariel Szmerla <aszmerla@g.jct.ac.il>
2021-12-19 10:20:15 +0200

    c

--- PL1/Converter.cs
+++ PL1/Converter.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Data;
-
 namespace PL
 {
     public sealed class ConverterDoubleToInt : IValueConverter
--- PL1/DroneListWindow1.xaml.cs
+++ PL1/DroneListWindow1.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using BLAPI;
 using BO;
+
+
 namespace PL
 {
 
