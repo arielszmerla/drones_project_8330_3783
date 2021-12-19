f56bb56a072a76f8b1a899581a693c19dbd3afe8
Ariel Szmerla <aszmerla@g.jct.ac.il>
2021-12-19 10:55:42 +0200

    xs

--- BL1/Enums.cs
+++ BL1/Enums.cs
@@ -13,7 +13,7 @@ namespace BO
         public enum BaseStationEmplacemt { TelAviv, PetachTikva, Holon, BneBrak, RamatGan }
         public enum DroneEmplacement { TelAvivCentral, TelAvivAzrieli, BneBrak, Holon, TelAvivUniversity, RamatGanCenter, PetacTikva, BarIlan, TelAvivDizengoff, Ponevezh }
         public enum DroneNames { PHANTOM, MAVIC, RIZY }
-        public enum WeightCategories { Light, Medium, Heavy, Unknown }
+        public enum WeightCategories { Light, Medium, Heavy}
         public enum DroneStatuses { Vacant, Maintenance, InDelivery }
         public enum ParcelStatus { Created, Assigned, PickedUp, Delivered }
         public enum Priorities { Normal, Fast, Urgent }
